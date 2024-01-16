9c2b8a10970df14f1198227b0e6f27068d0114ff
WellDiniz19 <wellingtondiniz84@gmail.com>
2024-01-15 22:40:22 -0300

    Realizando a configuração do TimeZone e Locale do projeto

---
+++ src/main/java/br/com/dinizbit/dnzparking/DnzparkingApplication.java
@@ -0,0 +1,13 @@
+package br.com.dinizbit.dnzparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnzparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnzparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dinizbit/dnzparking/DnzparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dinizbit.dnzparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnzparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
