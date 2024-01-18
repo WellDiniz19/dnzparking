4d056bdae63afc10ff0a6ebc43ced39470ee5f2e
WellDiniz19 <wellingtondiniz84@gmail.com>
2024-01-18 07:19:19 -0300

    Criando classe de entidasde usuario

---
+++ src/main/java/br/com/dinizbit/dnzparking/entity/User.java
@@ -0,0 +1,59 @@
+package br.com.dinizbit.dnzparking.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long user_Id;
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 30)
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+    @Column(name = "created_by")
+    private String created_by;
+    @Column(name = "modified_by")
+    private String modified_by;
+
+    public enum Role{
+        ROLE_ADMIN, ROLE_CLIENT
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(user_Id, user.user_Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user_Id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_Id=" + user_Id +
+                '}';
+    }
+}
