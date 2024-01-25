9908b455514c53297ab2bcd0ee09f335cee1ad63
WellDiniz19 <wellingtondiniz84@gmail.com>
2024-01-24 22:22:52 -0300

    Adicionando recurso de criação de novos usuários

--- src/main/java/br/com/dinizbit/dnzparking/service/UserService.java
+++ src/main/java/br/com/dinizbit/dnzparking/service/UserService.java
@@ -1,12 +1,18 @@
 package br.com.dinizbit.dnzparking.service;
 
+import br.com.dinizbit.dnzparking.entity.User;
 import br.com.dinizbit.dnzparking.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
+  @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/br/com/dinizbit/dnzparking/web/controller/UserController.java
+++ src/main/java/br/com/dinizbit/dnzparking/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package br.com.dinizbit.dnzparking.web.controller;
 
+import br.com.dinizbit.dnzparking.entity.User;
 import br.com.dinizbit.dnzparking.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> created (@RequestBody User user){
+        User usuario = userService.save(user);
+    return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
 }
