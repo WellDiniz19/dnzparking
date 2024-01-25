1d7f4ade9c43aa580e60ad9935c45c9447318046
WellDiniz19 <wellingtondiniz84@gmail.com>
2024-01-24 23:43:41 -0300

    Adicionando recurso de lista todos os usuários

--- src/main/java/br/com/dinizbit/dnzparking/service/UserService.java
+++ src/main/java/br/com/dinizbit/dnzparking/service/UserService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -29,4 +31,9 @@ public class UserService {
         usuario.setPassword(password);
         return usuario;
     }
+
+    @Transactional(readOnly = true)
+    public List<User> searchAll() {
+        return userRepository.findAll();
+   }
 }
--- src/main/java/br/com/dinizbit/dnzparking/web/controller/UserController.java
+++ src/main/java/br/com/dinizbit/dnzparking/web/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
@@ -15,19 +17,24 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> created (@RequestBody User user){
+    public ResponseEntity<User> created(@RequestBody User user){
         User usuario = userService.save(user);
     return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
 
     @GetMapping("/{user_id}")
-    public ResponseEntity<User> getById (@PathVariable Long user_id){
+    public ResponseEntity<User> getById(@PathVariable Long user_id){
         User usuario = userService.searchById(user_id);
         return ResponseEntity.ok(usuario);
     }
     @PatchMapping("/{user_id}")
-    public ResponseEntity<User> updatePassword (@PathVariable Long user_id, @RequestBody User user){
+    public ResponseEntity<User> updatePassword(@PathVariable Long user_id, @RequestBody User user){
         User usuario = userService.editPassword(user_id, user.getPassword());
         return ResponseEntity.ok(usuario);
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        List<User> usuarios = userService.searchAll();
+        return ResponseEntity.ok(usuarios);
     }
 }
